4d23204ba715b9fb80aa29cfb10c87cf911e1ecc
Manish Gopal Singh <manish.singh707@gmail.com>
2015-08-05 22:00:13 +0545

    updated form fields

--- resources/views/post/form.blade.php
+++ resources/views/post/form.blade.php
@@ -1,6 +1,19 @@
 <div class="form-group">
-    {!! Form::label('title', 'title', ['class'=>'col-sm-2 control-label'])!!}
+    {!! Form::label('title', 'Title:', ['class'=>'col-sm-2 control-label'])!!}
     <div class="col-sm-7">
         {!! Form::text('title',null, ["class"=>"form-control"])!!}
+    </div>
+</div>
+
+<div class="form-group">
+    {!! Form::label('description', 'Description:', ['class'=>'col-sm-2 control-label'])!!}
+    <div class="col-sm-7">
+        {!! Form::textarea('description',null, ["class"=>"form-control"])!!}
+    </div>
+</div>
+
+<div class="form-action">
+    <div class="col-sm-7 col-lg-offset-2">
+        {!! Form::submit('Submit',['class'=>'btn pull-right btn-primary']) !!}
     </div>
 </div>
