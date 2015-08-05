7f75bbf89ed38c31d7be9cbad3633cb5326bcafc
Manish Gopal Singh <manish.singh707@gmail.com>
2015-08-05 20:25:08 +0545

    added post model

---
+++ app/Models/Post/Post.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Models\Post;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property  title
+ * @property  description
+ */
+class Post extends Model {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'posts';
+}
